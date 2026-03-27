9ec03837b68ef19cc901c11c3831b541593e3800
meichel120500 <144088372+meichel120500@users.noreply.github.com>
2026-03-27 17:42:34 +0700

    Update DatabaseSeeder.php
     untuk memanggil UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        // TAMBAHKAN baris ini di bawahnya untuk memanggil UserSeeder
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
